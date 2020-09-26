695eed5d1fe035a74dea56201fc8285dde9d4442
rahulbordoloi <rahulbordoloi24@gmail.com>
2020-09-26 19:07:08 +0530

    Rough 'Main'

--- main.py
+++ main.py
@@ -1,87 +1,104 @@
+# Importing Libraries
+
 import shutil
 from pathlib import Path
 import os
 from os import listdir
 from os.path import isfile, join
 
-#Set home directory path
-#homedir = os.environ['HOME']
-#print (homedir)
+# Setting Home Directory Path
+
+## homedir = os.environ['HOME']
+## print (homedir)
 homedir = str(Path.home())
 
-#Set the soource direcory path i.e the Downloads path
-#Change the "/Downloads" path to any other appropriate path if this script is required to run in that directory
-source = os.path.join(homedir,'Downloads')
-all_files= [f for f in listdir(source) if isfile(join(source, f))]
+# Set the Source Direcory Path [i.e the Downloads Path]
 
-#List containing the files to be moved
-documents=[]
-pictures=[]
-musics=[]
+## Change the "/Downloads" path to any other appropriate path if this script is required to run in that Directory
+source = os.path.join(homedir, 'Downloads')
+all_files = [f for f in listdir(source) if isfile(join(source, f))]
 
-#ALl file extensions
-#Add file extension in required places to include that files in that directory
-picture_ext=('.jpeg','.jpg','.png','.bnp','.tiff')
-document_ext=('.txt','.pdf','.doc','.odt','.docx','.xls','.xlsx','.tex','.ppt','.rtf','.wpd')
-music_ext=('.mp3','.mp4','.mpeg','.wav','.wma','.3gp','.avi','.mkv','.mov')
+# List Containing the files to be Moved
 
-#Searching and categorising
+documents = []
+pictures = []
+media = []
+
+# Including All Types of File Extensions Possible
+
+## Adding file extension in required places to include that files in that directory
+picture_ext = ['.jpeg', '.jpg', '.png', '.bnp', '.tiff']
+document_ext = ['.txt', '.md', '.pdf', '.doc', '.odt', '.docx', '.xls', '.xlsx', '.csv', '.tex', '.ppt', '.pptx', '.rtf', '.wpd']
+media_ext = ['.mp3', '.mp4', '.mpeg', '.wav', '.wma', '.3gp', '.avi', '.mkv', '.mov']
+
+## Adding CAPS Extensions too
+picture_ext += [i.upper() for i in picture_ext]
+document_ext += [i.upper() for i in picture_ext]
+media_ext += document_ext
+
+# Searching for Files and Categorising
+
 for files in all_files:
     if files.endswith(picture_ext):
         pictures.append(files)
     elif files.endswith(document_ext):
         documents.append(files)
-    elif files.endswith(music_ext):
-        musics.append(files)
+    elif files.endswith(media_ext):
+        media.append(files)
 
-#Functions to show moved files
+# Display Functions to Print Operation [Show Moved Files]
+
 def show_moved_documents():
-    print("Files moved to Documents:")
+    print(f"{len(documents)} Files moved to Documents!")
     for document in documents:
         print (document)
 
 def show_moved_pictures():
-    print("Files moved to Pictures:")
+    print(f"{len(pictures)} Files moved to Pictures!")
     for picture in pictures:
         print (picture)
 
-def show_moved_musics():
-    print("Files moved to Musics:")
-    for music in musics:
+def show_moved_media():
+    print(f"{len(media)} Files moved to Media!")
+    for music in media:
         print (music)
 
-#functions handling the moving mechanism
+# Functions Handling the Moving Mechanism
+
 def move_to_document():
-    destination = os.path.join(homedir,'Documents')
+    destination = os.path.join(homedir, 'Documents')
+    print("Moving Documents ...")
     for document in documents:
-        shutil.move(os.path.join(source,document),destination)
+        shutil.move(os.path.join(source, document), destination)
     show_moved_documents()
 
 def move_to_pictures():
-    destination = os.path.join(homedir,'Pictures')
+    destination = os.path.join(homedir, 'Pictures')
+    print("Moving Pictures ...")
     for picture in pictures:
-        shutil.move(os.path.join(source,picture),destination)
+        shutil.move(os.path.join(source, picture), destination)
     show_moved_pictures()
 
-def move_to_musics():
-    destination = os.path.join(homedir,'Music')
-    for music in musics:
-        shutil.move(os.path.join(source,music),destination)
-    show_moved_musics()
+def move_to_media():
+    destination = os.path.join(homedir, 'Media')
+    print("Moving Media Files ...")
+    for music in media:
+        shutil.move(os.path.join(source, music), destination)
+    show_moved_media()
 
+# Condition to Check if Required Files are Present [for Moving] and are Calling Appropriate Functions
 
-#Condition to check if required files are present and call appropriate functions
-if len(documents)!=0:
+if len(documents) != 0:
     move_to_document()
 else:
-    print("No Documents to move")
+    print("No Documents to Move!")
 
-if len(pictures)!=0:
+if len(pictures) !=0:
     move_to_pictures()
 else:
-    print("No Pictures to move")
+    print("No Pictures to Move!")
 
-if len(musics)!=0:
-    move_to_musics()
+if len(media) != 0:
+    move_to_media()
 else:
-    print("No Music to move")
+    print("No Media Files to Move!")
