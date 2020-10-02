5a7c1afd7e51b9b5c7c23557ac03c4eb1ccd57b6
rahulbordoloi <rahulbordoloi24@gmail.com>
2020-10-03 02:13:38 +0530

    Null Input Check [Update] [OOPS] [Unit Testing]

---
+++ Test/Unit Testing/nullInput.py
@@ -0,0 +1,24 @@
+# Null Input
+
+print('''
+Where do you want to Organise your Files?
+1. Downloads
+2. Any Other Location
+Enter the Full Path to your Directory [Just Type 'Downloads' for Case 2] ->
+''')
+
+path = input()
+if not path:
+    print(f"None!")
+else:
+    print(f"Not Null!")
+
+path = None
+if not path:
+    path = "Downloads"
+else:
+    path = "Not Downloads"
+print(path)
+
+
+## Conlusion => Unit Testing Successful for nullInput.
